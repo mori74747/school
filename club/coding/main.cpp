f576026b0181a2cabb544537d921f5caee0f40b0
happaniki <mori74747@gmail.com>
2026-05-08 10:50:18 +0900

    commit

--- club/coding/main.cpp
+++ club/coding/main.cpp
@@ -1,41 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-const ll INF = 1e18;
-
 
 int main(){
+	int n;
+	int ans=0;
 
-	// *** 入力の高速化 *** //
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+	cin >> n;
 
-	// *** コード部分 *** //
-
-	// 入力
-	int n, x;	cin >> n >> x;
-
-	vector<int> a(n+1);
-	for(int i = 1; i <= n; i++){
-		cin >> a[i];
-	}
-
-	int l = 1;
-	int r = n;
-	while(l <= r){
-		int m = (r + l) / 2;
-
-		if(x == a[m]){
-			cout << m << "\n";
-			break;
-		}
-		if(x > a[m]){
-			l = m + 1;
-		}
-		if(x < a[m]){
-			r = m - 1;
+	for (int i=1; i<=n; i++)
+	{
+		if (n % i == 0)
+		{
+			ans++;
 		}
 	}
 
+	if (ans <= 2)
+	{
+		cout << 1 << endl;
+	}
+	else
+	{
+		cout << 2 << endl;
+	}
 	return (0);
 }
