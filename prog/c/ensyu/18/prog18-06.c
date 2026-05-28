093d9b3f0f1a0efcb2469e587c73893ee9cec6ff
happaniki <mori74747@gmail.com>
2026-05-28 10:44:02 +0900

    commit

--- prog/c/ensyu/18/prog18-06.c
+++ prog/c/ensyu/18/prog18-06.c
@@ -15,7 +15,7 @@ main()
   char buff[256];
   Fraction a, b, c;
 
-  a = getFraction();   // 分数aをキーボードより入力する
+  a = getFraction(); // 分数aをキーボードより入力する
 
   printf("a = ");
   frcPrint(a);         // 入力した分数aを表示
@@ -37,10 +37,16 @@ main()
 }
 
 // キーボードより、a/bの形で分数を入力する
-// [引　数] msg[] : 入力時に表示するメッセージ
 // [戻り値] Fraction型で表現される分数
 Fraction getFraction(void)
 {
+  char buff[256];
+  fgets(buff, sizeof(buff), stdin);
+  
+  Fraction frac_input;
+  sscanf(buff, "%d/%d", &frac_input.numerator, &frac_input.denominator);
+
+  return(frac_input);
 }
 
 // 分子と分母の値からFraction型で表される分数を返す
@@ -49,12 +55,19 @@ Fraction getFraction(void)
 // [戻り値] Fraction型で表現される分数
 Fraction frcCreate(int numerator, int denominator)
 {
+  Fraction frac_create;
+  frac_create.numerator   = numerator;
+  frac_create.denominator = denominator;
+
+  return(frac_create);
 }
 
 // 分数xを a/b の形で表示する
 // [引　数] x : 表示する分数
 void frcPrint(Fraction x)
 {
+  printf("%d/%d", x.numerator, x.denominator);
+  return;
 }
 
 // 分数xとyの乗算結果を返す
@@ -62,4 +75,9 @@ void frcPrint(Fraction x)
 // [戻り値] x * y を計算して得られる分数
 Fraction frcMul(Fraction x, Fraction y)
 {
+  Fraction frac_mul;
+  frac_mul.numerator   = x.numerator   * y.numerator;
+  frac_mul.denominator = x.denominator * y.denominator;
+
+  return(frac_mul);
 }
