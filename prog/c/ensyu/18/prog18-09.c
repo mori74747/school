52408f59372f8188af3748e298b32752fed138cc
happaniki <mori74747@gmail.com>
2026-05-29 11:20:29 +0900

    commit

--- prog/c/ensyu/18/prog18-09.c
+++ prog/c/ensyu/18/prog18-09.c
@@ -5,19 +5,19 @@ typedef struct {
   int denominator; // 分母
 } Fraction;
 
+int frcGetNumerator(Fraction x);
+int frcGetDenominator(Fraction x);
 Fraction getFraction(void);
 Fraction frcCreate(int numerator, int denominator);
 Fraction frcMul(Fraction x, Fraction y);
 void frcPrint(Fraction x);
-int frcGetNumerator(Fraction x);
-int frcGetDenominator(Fraction x);
 
 main()
 {
   char buff[256];
   Fraction a, b, c;
 
-  a = getFraction();   // 分数aをキーボードより入力する
+  a = getFraction(); // 分数aをキーボードより入力する
 
   printf("a = ");
   frcPrint(a);         // 入力した分数aを表示
@@ -43,7 +43,7 @@ main()
 // [戻り値] 分子の値
 int frcGetNumerator(Fraction x)
 {
-  return(           );
+  return(x.numerator);
 }
 
 // Fraction型で表される分数の分母の値を返す
@@ -51,55 +51,52 @@ int frcGetNumerator(Fraction x)
 // [戻り値] 分母の値
 int frcGetDenominator(Fraction x)
 {
-  return(             );
+  return(x.denominator);
 }
 
 // キーボードより、a/bの形で分数を入力する
-//   [引　数] msg[] : 入力時に表示するメッセージ
-//   [戻り値] Fraction型で表現される分数
+// [戻り値] Fraction型で表現される分数
 Fraction getFraction(void)
 {
   char buff[256];
-  int n, d;
-  
   fgets(buff, sizeof(buff), stdin);
-  sscanf(buff, "%d / %d", &n, &d);
   
-  return(               );
+  int z_numerator, z_denominator;
+  sscanf(buff, "%d/%d", &z_numerator, &z_denominator);
+
+  return(frcCreate(z_numerator, z_denominator));
 }
 
 // 分子と分母の値からFraction型で表される分数を返す
-//   [引　数] numerator   : 分子の値
-//            denominator : 分母の値
-//   [戻り値] Fraction型で表現される分数
+// [引　数] numerator   : 分子の値
+//          denominator : 分母の値
+// [戻り値] Fraction型で表現される分数
 Fraction frcCreate(int numerator, int denominator)
 {
-  Fraction x;
+  Fraction frac_create;
+  frac_create.numerator   = numerator;
+  frac_create.denominator = denominator;
 
-                = numerator;
-                = denominator;
-
-  return( );
+  return(frac_create);
 }
 
 // 分数xを a/b の形で表示する
-//   [引　数] x : 表示する分数
+// [引　数] x : 表示する分数
 void frcPrint(Fraction x)
 {
-  printf("%d/%d",                      ,                     );
-
+  printf("%d/%d", frcGetNumerator(x), frcGetDenominator(x));
   return;
 }
 
 // 分数xとyの乗算結果を返す
-//   [引　数] x, y : 乗算対象となる分数
-//   [戻り値] x * y を計算して得られる分数
+// [引　数] x, y : 乗算対象となる分数
+// [戻り値] x * y を計算して得られる分数
 Fraction frcMul(Fraction x, Fraction y)
 {
-  int n, d;
 
-  n =                                            ;
-  d =                                            ;
+  int z_numerator, z_denominator;
+  z_numerator   = frcGetNumerator(x)   * frcGetNumerator(y);
+  z_denominator = frcGetDenominator(x) * frcGetDenominator(y);
 
-  return(               );
+  return(frcCreate(z_numerator, z_denominator));
 }
