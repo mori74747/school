e3ea5411748cc5f81b5f7b0c1cb48cd8235c54d2
happaniki <mori74747@gmail.com>
2026-06-02 12:35:06 +0900

    commit

--- private/test.c
+++ private/test.c
@@ -3,35 +3,33 @@
 int main(){
 
 	int x, n;
-	scanf("%d%d",&x,&n);
+	scanf("%d%d", &x, &n);
 
 	int c[n];
 	for(int i = 0; i < n; i++){
-		scanf("%d",&c[i]);
+		scanf("%d", &c[i]);
 	}
-	int left  = 0;
-	int right = 0;
 
-	int sum = c[0];
-	int max = 0;
+	int left = 0;
+	long long sum = 0;
+	int ans = 0;
 
-	while(right < n){
-		if(sum <= x){
-			if(max < (right - left + 1)){
-				max = right - left + 1;
-			}
-			if(right + 1 < n){
-				right++;
-				sum += c[right];
-			}else{
-				break;
-			}
-		}else{
+	for(int right = 0; right < n; right++){
+
+		sum += c[right];
+
+		while(sum > x){
 			sum -= c[left];
 			left++;
 		}
-	}
-	printf("%d\n",max);
 
-	return (0);
+		int len = right - left + 1;
+		if(ans < len){
+			ans = len;
+		}
+	}
+
+	printf("%d\n", ans);
+
+	return 0;
 }
