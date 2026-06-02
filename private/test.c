f95bbca95bc9f942a8f46916b1cbcd5d105e7eda
happaniki <mori74747@gmail.com>
2026-06-02 17:27:38 +0900

    commit

--- private/test.c
+++ private/test.c
@@ -4,7 +4,7 @@ int main(){
 
 	// 変数の入力
 	int x, n;
-	scanf("%d%d", &x, &n);
+	scanf("%d %d", &x, &n);
 
 	int c[n];
 	for(int i = 0; i < n; i++){
@@ -14,25 +14,31 @@ int main(){
 	// 尺取り法の初期化
 	int left = 0;
 	long long sum = 0;
-	int ans = 0;
+	int ans = n+1;
 
 	// 尺取り法手順
 	for(int right = 0; right < n; right++){
 
 		sum += c[right];
 
-		while(sum > x){
+		while(sum >= x){
+
+			// 最大長から最短長になると、更新タイミングが変わる
+			// 更新した後に広げる動作を行う
+			int len = right - left + 1;
+			if(len < ans){
+				ans = len;
+			}
 			sum -= c[left];
 			left++;
 		}
-
-		int len = right - left + 1;
-		if(ans < len){
-			ans = len;
-		}
+	}
+	// ans が更新されていなければ-1を出力
+	if(ans == n + 1){
+		printf("-1\n");
+	}else{
+		printf("%d\n", ans);
 	}
 
-	printf("%d\n", ans);
-
-	return 0;
+	return (0);
 }
