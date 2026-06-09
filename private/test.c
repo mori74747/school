819505f6de74a6abaf9739e73cd8f42fdff13f62
happaniki <mori74747@gmail.com>
2026-06-09 12:25:28 +0900

    commit

--- private/test.c
+++ private/test.c
@@ -1,44 +1,33 @@
 #include <stdio.h>
 
-int main(){
+int main() {
 
-    // 変数の入力
-    long long x, n;
-    scanf("%lld %lld", &x, &n);
+    int n;
+    scanf("%d",&n);
 
-    int c[n];
+    int a[n];
     for(int i = 0; i < n; i++){
-        scanf("%d", &c[i]);
+        scanf("%d",&a[i]);
     }
 
-    // 尺取り法の初期化
-    int left = 0;
-    long long sum = 0;
-    int ans = n+1;
-
-    // 尺取り法手順
-    for(int right = 0; right < n; right++){
-
-        sum += c[right];
-
-        // xがsum以上である（条件を満たしている間ループ)
-        while(sum >= x){
-
-            // 条件を満たしている間はできる限り縮めたい
-            int len = right - left + 1;
-            if(len < ans){
-                ans = len;
+    // バブルソート
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] > a[j]) {
+                int tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
             }
-            sum -= c[left];
-            left++;
         }
     }
-    // ans が更新されていなければ-1を出力
-    if(ans == n + 1){
-        printf("-1\n");
-    }else{
-        printf("%d\n", ans);
+
+    // 重複を飛ばしながら出力
+    printf("%d\n", a[0]);
+    for (int i = 1; i < n; i++) {
+        if (a[i] != a[i-1]) {
+            printf("%d\n", a[i]);
+        }
     }
 
-    return (0);
+    return 0;
 }
---
+++ prog/c/ensyu/19/prog19.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef struct {
+    double x; // x 座標 
+    double y; // y 座標 
+} Point;
+
+typedef struct {
+    Point p1; // 対角線を表す点1 
+    Point p2; // 対角線を表す点2 
+} Rect;
+
+double ptGetX(Point p);
+double ptGetY(Point p);
+Point ptCreate(double x, double y);
+
+double rctArea(Rect rct);
+double rctPerim(Rect rct);
+double rctWidth(Rect rct);
+double rctHeight(Rect rct);
+double rctDiaglen(Rect rct);
+Point rctTopLeftCorner(Rect rct);
+Rect rctCreate1(Point p1, Point p2);
+Rect rctCreate2(Point p, double width, double height);
+
+main()
+{
+    Rect r;
+    Point p1, p2;
+    double x1, y1, x2, y2, w, h, s;
+
+    printf("長方形１\n");
+    printf("対角線の端点1の座標(x, y)："); scanf("%lg,%lg", &x1, &y1);
+    printf("対角線の端点2の座標(x, y)："); scanf("%lg,%lg", &x2, &y2);
+    p1 = ptCreate(x1, y1);
+    p2 = ptCreate(x2, y2);
+
+    r = rctCreate1(p1, p2); // 長方形の対角線を表す２点で初期化 
+    putchar('\n');
+    printf("左上隅の座標(x, y)：(%g, %g), ", ptGetX(rctTopLeftCorner(r)), ptGetY(rctTopLeftCorner(r)));
+    printf("面積：%g, 周囲：%g, 対角線長：%g\n", rctArea(r), rctPerim(r), rctDiaglen(r));
+    putchar('\n');
+
+    printf("長方形２\n");
+    printf("左上の点の座標(x, y)："); scanf("%lg,%lg", &x1, &y1);
+    p1 = ptCreate(x1, y1);
+    printf("横："); scanf("%lg", &w);
+    printf("縦："); scanf("%lg", &h);
+
+    r = rctCreate2(p1, w, h); // 長方形の左上の点と、幅および高さで初期化 
+    putchar('\n');
+    printf("左上隅の座標(x, y)：(%g, %g), ", ptGetX(rctTopLeftCorner(r)), ptGetY(rctTopLeftCorner(r)));
+    printf("面積：%g, 周囲：%g, 対角線長：%g\n", rctArea(r), rctPerim(r), rctDiaglen(r));
+
+    return(0);
+}
+
+// point型の操作関数
+
+// 点pのx座標を返す
+// [引　数] Point型 : p
+// [戻り値] double型: 点pのx座標
+double ptGetX(Point p){
+    return(p.x);
+}
+
+// 点pのy座標を返す
+// [引　数] Point型 : p
+// [戻り値] double型: 点pのy座標
+double ptGetY(Point p){
+    return(p.y);
+}
+
+// 座標(x, y)の点を生成する
+// [引　数] double型: x, y
+// [戻り値] Point型 : 座標(x, y)の点
+Point ptCreate(double x, double y){
+    Point z;
+    z.x = x;
+    z.y = y;
+
+    return(z);
+}
+
+// Rect型の操作関数
+
+// 長方形rctの面積を返す
+// [引　数] Rect型  : 長方形rct
+// [戻り値] double型: 長方形rctの面積
+double rctArea(Rect rct){
+    
+}
+
+// 長方形rctの周囲の長さを返す
+// [引　数] Rect型  : 長方形rct
+// [戻り値] double型: 長方形rctの周囲の長さ
+double rctPerim(Rect rct){
+
+}
+
+// 長方形rctの幅(横)の長さを返す
+// [引　数] Rect型  : 長方形rct
+// [戻り値] double型: 長方形rctの幅(横)の長さ
+double rctWidth(Rect rct){
+    
+}
+
+// 長方形rctの高さ(縦)の長さを返す
+// [引　数] Rect型  : 長方形rct
+// [戻り値] double型: 長方形rctの高さ(縦)の長さ
+double rctHeight(Rect rct){
+
+}
+
+// 長方形rctの対角線の長さを返す
+// [引　数] Rect型  : 長方形rct
+// [戻り値] double型: 長方形rctの対角線の長さ
+double rctDiaglen(Rect rct){
+
+}
+
+// 長方形rctの左上隅の点を返す
+// [引　数] Rect型  : 長方形rct
+// [戻り値] Point型 : 長方形rctの左上隅の点
+Point rctTopLeftCorner(Rect rct){
+    return(rct.p1);
+}
+
+// 対角線を表す2点p1とp2から、長方形を生成する
+// [引　数] Point型 : 対角線を表す p1, p2
+// [戻り値] Rect型  : p1とp2が対角線上にある長方形
+Rect rctCreate1(Point p1, Point p2){
+    Rect z;
+    z.p1.x = p1.x;
+    z.p1.y = p1.y;
+    z.p2.x = p2.x;
+    z.p2.y = p2.y;
+
+    return (z);
+}
+
+// 左上隅の1点pと、幅(横)widthおよび高さ(縦)heightの長さから、長方形を生成する
+// [引　数] Point型 : 左上隅の点p ,
+//  　　　  double型: 幅(横)の長さwidth, 高さ(縦)の長さheight
+// [戻り値] Rect型  : p1とp2が対角線上にある長方形
+Rect rctCreate2(Point p, double width, double height){
+    Rect z;
+    z.p1.x = p.x;
+    z.p1.y = p.y;
+    
+}
+
