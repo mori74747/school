71417b435b7fa1acc19ea4ca441b3454f4ee69ee
happaniki <mori74747@gmail.com>
2026-09-10 09:58:02 +0900

    commit

--- kouki/c-lang/7/toi/toi19-01.c
+++ kouki/c-lang/7/toi/toi19-01.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
-    int i;  //  カウンタ変数  
+    int i;              //  カウンタ変数
+    int count;          //  個数の受け取り変数
+    char name = "";     //  商品名の格納用変数
+    int prise;          //  商品値段の格納用変数
+    int total = 0;      //  合計金額格納用変数
+    int tax;            //  消費税格納用変数
 
     //  オプションの有無・正誤のチェック  
     if(argc <= 1 || argv[1][0] != '-'){
@@ -14,24 +19,36 @@ int main(int argc, char *argv[]){
         *    またはハイフンのないオプションにであったら
         *    ループから抜ける
         */
-        i = 1;  //  カウンタ変数の初期化  
+        i = 1;  //  カウンタ変数の初期化
         while(argc > i && argv[i][0] == '-' ){
+            if(argv[i][1] >= '0' && argv[i][1] <= '9'){
+                sscanf(&argv[i][1], "%d", &tax);
+                break;
+            }
             switch(argv[i][1]){
                 case 'a':
-                    printf("りんご\n");
+                    name = "りんご";
+                    prise = 100;
                     break;
                 case 'b':
-                    printf("バナナ\n");
+                    name = "ばなな";
+                    prise = 30;
                     break;
                 case 'c':
-                    printf("さくらんぼ\n");
+                    name = "さくらんぼ";
+                    prise = 200;
                     break;
                 case 'd':
-                    printf("L");
+                    name = "どんぐり";
+                    prise = 50;
                     break;
+                case 'e':
+                    name = "えび";
+                    prise = 1000;
                 default:
                     printf("オプションとして使用できません。\n");
             }
+            sscanf(&argv[i][2], "%d", &count);
             i++;
         }
     }
