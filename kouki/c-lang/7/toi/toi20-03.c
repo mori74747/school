5730f1e698be924079eba9cf8e79b96a3fc2706d
happaniki <mori74747@gmail.com>
2026-09-15 12:18:58 +0900

    commit

--- kouki/c-lang/7/toi/toi20-03.c
+++ kouki/c-lang/7/toi/toi20-03.c
@@ -9,8 +9,7 @@ main(){
   char **p;       //  文字列
   
   char buff[256]; //  入力用バッファ文字列
-  int i;          //  カウンタ変数
-  int error = 0;  //  エラーの有無（[0]...正常終了 [1]...エラー）
+  int i, j;       //  カウンタ変数
 
 
   n = getint("文字列の数 n: ");
@@ -20,34 +19,28 @@ main(){
   if(p == NULL){
     printf("メモリ確保エラー\n");
     return (1);
-  }else{
-    for(i = 0; i < n; i++){
-      //  文字列の入力
-      fgets(buff, sizeof(buff), stdin);
-      //  改行文字の排除
-      buff[strcspn(buff, "\n")] = '\0';
-      //  文字列を動的に確保
-      p[i] = (char*)malloc((strlen(buff)+1) * sizeof(char));
-      //  メモリ確保の失敗
-      if(p[i] == NULL){
-        error = 1;
-      }else{
-        //  文字列を確保したメモリに格納
-        strcpy(p[i], buff);
-      }
-    }
   }
-  //  メモリ確保の失敗
-  if(error == 1){
-    printf("メモリ確保エラー\n");
-    // 確保したメモリの開放
-    if(p != NULL){
-      for(i = 0; i < n; i++){
-        free(p[i]);
+
+  for(i = 0; i < n; i++){
+    //  文字列の入力
+    fgets(buff, sizeof(buff), stdin);
+    //  改行文字の排除
+    buff[strcspn(buff, "\n")] = '\0';
+    //  文字列を動的に確保
+    p[i] = (char*)malloc((strlen(buff)+1) * sizeof(char));
+    //  メモリ確保の失敗
+    if(p[i] == NULL){
+      printf("メモリ確保エラー\n");
+      //  既に確保したメモリの開放
+      for(j = 0; j < i; j++){
+        free(p[j]);
       }
       free(p);
+
+      return (1);
     }
-    return(1);
+    //  文字列を確保したメモリに格納
+    strcpy(p[i], buff);
   }
 
   for(i = 0; i < n; i++){
